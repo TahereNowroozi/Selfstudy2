39ffbf1d6a1aaf0ea20c75ba642e445ac945f40c
TahereNowroozi <you@example.com>
2023-09-06 13:41:30 -0600

    Updated Selfstudy2.java

--- Selfstudy2.java
+++ Selfstudy2.java
@@ -3,6 +3,11 @@ public class Selfstudy2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+             int a = 10;
+             int b = 2;
+             int c = a/b;
+             System.out.println(c);
+
 
 	}
 
